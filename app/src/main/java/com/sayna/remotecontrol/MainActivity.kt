147482066b0795316b3f3f059d7215ebd6edcf6f
Seina <32441141+Nwott@users.noreply.github.com>
2024-05-23 11:37:53 -0400

    Created model class

--- app/src/main/java/com/sayna/remotecontrol/MainActivity.kt
+++ app/src/main/java/com/sayna/remotecontrol/MainActivity.kt
@@ -16,32 +16,14 @@ import com.sayna.remotecontrol.ui.theme.RemoteControlTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             RemoteControlTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }*/
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RemoteControlTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/sayna/remotecontrol/models/RemoteControlAction.kt
@@ -0,0 +1,7 @@
+package com.sayna.remotecontrol.models
+
+data class RemoteControlAction(
+    val id: String,
+    val title: String,
+    val irCode: String
+)
